791eb8367bc1ec15c2c776904941434bc1227cf5
Huy Nguyen <huy.nguyen.gl99@hcmut.edu.vn>
2020-04-19 08:58:20 +0700

    Add router for three main component

---
+++ src/views/Module/Authentication/LogIn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './theme/LogIn.scss'
+const LogIn = () => {
+  return (
+    <div>
+      Login Here
+      <div>Login</div>
+    </div>
+  );
+};
+
+export default LogIn;
