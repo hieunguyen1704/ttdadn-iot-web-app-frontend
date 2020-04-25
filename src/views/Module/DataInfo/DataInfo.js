91a31f9515776f696425c4e13494d5f8c3864b75
Huy Nguyen <huy.nguyen.gl99@hcmut.edu.vn>
2020-04-25 18:20:33 +0700

    Add react graph

--- src/views/Module/DataInfo/DataInfo.js
+++ src/views/Module/DataInfo/DataInfo.js
@@ -1,11 +1,46 @@
 import React from "react";
-import './theme/DataInfo.scss'
+import "./theme/DataInfo.scss";
+import { Chart } from "react-charts";
 
+const data = [
+  {
+    label: "Series 1",
+    data: [
+      [0, 1],
+      [1, 2],
+      [2, 4],
+      [3, 2],
+      [4, 7],
+    ],
+  },
+  {
+    label: "Series 2",
+    data: [
+      [0, 3],
+      [1, 1],
+      [2, 5],
+      [3, 6],
+      [4, 4],
+    ],
+  },
+];
+
+const axes = [
+  { primary: true, type: "linear", position: "bottom" },
+  { type: "linear", position: "left" },
+];
 const DataInfo = () => {
   return (
-    <div>
-      DataInfo Here
-      <div>DataInfo</div>
+    <div className="data-info-component">
+      <div
+        className="data-chart"
+        style={{
+          width: "400px",
+          height: "300px",
+        }}
+      >
+        <Chart data={data} axes={axes} tooltip />
+      </div>
     </div>
   );
 };
