87374a0a6ad76c297590a459aebf24474224a166
hieupropy1999 <hoanghieupro1999@gmail.com>
2020-04-26 17:20:52 +0700

    change setting icon

--- src/views/Dashboard/listItems.js
+++ src/views/Dashboard/listItems.js
@@ -2,18 +2,18 @@ import React from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import DashboardIcon from "@material-ui/icons/Dashboard";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import PeopleIcon from "@material-ui/icons/People";
+import SettingsIcon from '@material-ui/icons/Settings';
 import { navigate } from "@reach/router";
 
 export const mainListItems = (
   <div>
     <ListItem button onClick={() => navigate("user-config")}>
       <ListItemIcon>
-        <DashboardIcon />
+        <SettingsIcon />
       </ListItemIcon>
-      <ListItemText primary="Dashboard" />
+      <ListItemText primary="Setting" />
     </ListItem>
     <ListItem button onClick={() => navigate("data-info")}>
       <ListItemIcon>
