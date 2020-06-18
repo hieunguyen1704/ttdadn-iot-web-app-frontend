363ba4f92d29d51d0bb65a5fa3b3b1e1867bbabc
hieupropy1999 <hoanghieupro1999@gmail.com>
2020-06-18 16:45:20 +0700

    use bootstrap select

--- src/views/Module/DataInfo/Select/Select.js
+++ src/views/Module/DataInfo/Select/Select.js
@@ -1,59 +1,59 @@
+/* eslint-disable jsx-a11y/no-onchange */
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
+import './Select.scss';
+// import { makeStyles } from "@material-ui/core/styles";
+// import InputLabel from "@material-ui/core/InputLabel";
+// import MenuItem from "@material-ui/core/MenuItem";
+// import FormControl from "@material-ui/core/FormControl";
+// import Select from "@material-ui/core/Select";
 
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(3),
-    minWidth: 150,
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   formControl: {
+//     margin: theme.spacing(3),
+//     minWidth: 150,
+//   },
+// }));
 
 export default function ControlledOpenSelect(props) {
-  const classes = useStyles();
+  // const classes = useStyles();
   const [mode, setMode] = React.useState("5");
-  const [open, setOpen] = React.useState(false);
+  // const [open, setOpen] = React.useState(false);
 
   const handleChange = (event) => {
     setMode(event.target.value);
     props.changed(event.target.value);
   };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+  // const handleClose = () => {
+  //   setOpen(false);
+  // };
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+  // const handleOpen = () => {
+  //   setOpen(true);
+  // };
 
   return (
-    <div style={{display: "flex", justifyContent: "center"}}>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="mode-select-label">Select Mode</InputLabel>
-        <Select
-          labelId="mode-select-label"
-          id="mode-select"
-          open={open}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          value={mode}
-          onChange={handleChange}
-        >
-          <MenuItem value={"5"}>5 Min</MenuItem>
-          <MenuItem value={"10"}>10 Min</MenuItem>
-          <MenuItem value={"30"}>30 Min</MenuItem>
-          <MenuItem value={"60"}>Hour</MenuItem>
-          <MenuItem value={"300"}>5 Hour</MenuItem>
-          <MenuItem value={"720"}>12 Hour</MenuItem>
-          <MenuItem value={"1440"}>Day</MenuItem>
-          <MenuItem value={"10080"}>Week</MenuItem>
-          <MenuItem value={"302400"}>Month</MenuItem>
-        </Select>
-      </FormControl>
+    <div style={{ display: "flex", justifyContent: "center"}}>
+      <form id="form">
+        <div className="form-group">
+          <label htmlFor="mode-select">Select Mode</label>
+          <select
+            onChange={handleChange}
+            value={mode}
+            className="form-control"
+            id="mode-select"
+          >
+            <option value={"5"}>5 Min</option>
+            <option value={"10"}>10 Min</option>
+            <option value={"30"}>30 Min</option>
+            <option value={"300"}>Hour</option>
+            <option value={"720"}>12 Hour</option>
+            <option value={"1440"}>Day</option>
+            <option value={"10080"}>Week</option>
+            <option value={"302400"}>Month</option>
+          </select>
+        </div>
+      </form>
     </div>
   );
 }
